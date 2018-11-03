3c33ee139f29348220be7b5ad80cb9ca128d469e
Wadah Esam <wadah-wleed@hotmail.com>
2018-11-03 11:22:02 +0300

    changing the style of the complaints list tap and convert it to cards instead of list items

--- src/Complaints/ComplaintListTap.js
+++ src/Complaints/ComplaintListTap.js
@@ -1,37 +1,61 @@
 import React, { Component } from 'react';
-import { ActivityIndicator } from 'react-native';
-import { View, Container, Header, Content, List, ListItem, Thumbnail, Text, Left, Body, Right, Button, Icon } from 'native-base';
+import { ActivityIndicator, StyleSheet } from 'react-native';
+import { View, Container, Header, Content, List, ListItem, Thumbnail, Text, Left, Body, Right, Button, Icon, CardItem, Card, Badge } from 'native-base';
 
 class ComplaintListTap extends Component {
+
   renderListItems = () => {
+
+
     if (this.props.complaints != null) {
       return this.props.complaints.slice(0).reverse().map((complaint) => {
         const isClosed = complaint.replay !== null;
-        return (
-          <ListItem key={complaint.id} thumbnail androidRippleColor>
-            <Left style={{width: 30}}>
-              {(isClosed) ? 
-                <Icon style={{color: '#B8BCBC', fontSize: 30, width: 30 }} name="md-lock" />
-              : 
-                <Icon style={{color: '#4050B5', fontSize: 30, width: 30 }} name="md-pulse" />
-              }
-              
-            </Left>
+        const statusClosedLabel = (
+          <Badge danger style={{position: 'relative', right: 3}}>
+            <Text style={{fontSize: 13}}>Closed</Text>
+          </Badge>
+        );
+        const statusOpenLabel = (
+          <Badge primary style={{position: 'relative', right: 3}}>
+            <Text style={{fontSize: 13}}>Open</Text>
+          </Badge>
+        );
+        const closedIcon = <Icon style={{color: '#D9544E', fontSize: 30, width: 30 }} name="md-lock" />;
+        const openIcon = <Icon style={{color: '#4050B5', fontSize: 30, width: 30 }} name="md-pulse" />;
+
+        const replayField = (
+          <CardItem footer bordered>
             <Body>
-              <Text>{complaint.title}</Text>
-              <Text note numberOfLines={1} style={{fontSize: 10}}>{complaint.created_at}</Text>
-              <Text note numberOfLines={7}>{complaint.description}</Text>
+              <Text style={{color: '#D9544E', fontWeight: '600'}}>Replay :</Text>
+              <Text note style={{color: '#D9544E'}}>{complaint.replay}</Text>
             </Body>
-            {(isClosed) ? 
+          </CardItem>
+        );
+        return (
+          <Card key={complaint.id}>
+            <CardItem header bordered>
+              <Left>
+                {(isClosed) ? closedIcon : openIcon}
+                <Body>
+                  <Text>{complaint.title}</Text>
+                  <Text note>{complaint.created_at}</Text>
+                </Body>
+              </Left>
+
               <Right>
-                <Button small style={{backgroundColor: "#B8BCBC"}} >
-                  <Text>Replay</Text>
-                </Button>
+                {(isClosed) ? statusClosedLabel : statusOpenLabel}
               </Right>
-              : 
-              <View />
-            }
-          </ListItem>
+            </CardItem>
+            <CardItem>
+              <Body>
+                <Text style={{fontWeight: '600'}}>Description :</Text>
+                <Text>
+                  {complaint.description}
+                </Text>
+              </Body>
+            </CardItem>
+            {(isClosed) ? replayField : <View/>}
+          </Card>
         );
       });
     } else {
@@ -40,10 +64,10 @@ class ComplaintListTap extends Component {
   }
 
   render() {
-    console.log(this.props.complaints);
+    // console.log(this.props.complaints);
     return (
       <Container>
-        <Content>
+        <Content padder>
           <List>
             {this.renderListItems()}
           </List>
@@ -52,5 +76,23 @@ class ComplaintListTap extends Component {
     )
   }
 }
+
+const styles = StyleSheet.create({
+  statusView: {
+    position: 'absolute',
+    top: 20,
+    right: 15,
+    height: 150,
+    backgroundColor: 'red',
+    paddingBottom: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 3,
+  },
+  statusText: {
+    fontSize: 12,
+    color: 'white',
+  }
+}); 
 
 export default ComplaintListTap;
