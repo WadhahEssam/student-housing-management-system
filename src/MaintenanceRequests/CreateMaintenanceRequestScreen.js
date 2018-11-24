8341da6bd9ad09e238ef0b29ddf0945ab4cd53b8
Wadhah Essam Mac <wadah-wleed@hotmail.com>
2018-11-24 15:12:06 +0300

    starting with the create maintenance request screnn

--- src/MaintenanceRequests/CreateMaintenanceRequestScreen.js
+++ src/MaintenanceRequests/CreateMaintenanceRequestScreen.js
@@ -1,36 +1,55 @@
 
 import React, { Component } from "react";
 import {
-  Text,
   View,
   StyleSheet,
   Image,
 } from 'react-native';
-import {
-  Icon,
-  Button,
-  Fab,
-  Container,
-} from 'native-base';
+import { Container, Content, Input, Item, Form, Textarea, Button, Text, Icon } from 'native-base';
+
 
 export default class CreateMaintenanceRequestScreen extends Component {
   state = {
     active: 'true',
+    title: '',
+    description: '',
+    isSubmitted: null,
   }
 
   render() {
+    console.log(this.state);
     return (
       <Container>
-        <Fab
-          active={this.state.active}
-          direction="up"
-          containerStyle={{ }}
-          style={{ backgroundColor: '#5067FF' }}
-          position="bottomRight"
-          onPress={() => this.setState({ active: !this.state.active })}
-        >
-          <Icon name="add" />
-        </Fab>
+        <Content padder>
+          <Form>
+            <Item regular>
+              <Input 
+                value={this.state.title} 
+                onChangeText={(title) => {this.setState({title})}} 
+                selectionColor="#4050B5" 
+                placeholder='Title' 
+              />
+            </Item>
+            <Textarea 
+              value={this.state.description} 
+              onChangeText={(description) => {this.setState({description})}} 
+              selectionColor="#4050B5" 
+              style={{paddingVertical: 5}} 
+              rowSpan={5} 
+              bordered 
+              placeholder="Description" 
+            />
+            <Button onPress={() => {this.submitComplaint()}} style={{marginTop: 10}} block success={this.state.isSubmitted} >
+              <Text>Submit</Text>
+            </Button>
+          </Form>
+          <View alignItems="cetner" style={styles.infoBox}>
+            <Icon style={{color: 'white'}} name="warning" />
+            <Text style={{color: 'white', fontSize: 12}}>
+              Housing adminstraion will recieve your complaint and replay to it soon after you submit it.
+            </Text>
+          </View>
+        </Content>
       </Container>
     );
   }
--- src/MaintenanceRequests/MaintenanceScreen.js
+++ src/MaintenanceRequests/MaintenanceScreen.js
@@ -10,6 +10,11 @@ import {
   Button,
   Fab,
   Container,
+  Header,
+  Left,
+  Right,
+  Body,
+  Segment
 } from 'native-base';
 
 export default class addmaintain extends Component {
@@ -20,6 +25,16 @@ export default class addmaintain extends Component {
   render() {
     return (
       <Container>
+        <Header hasSegment style={{paddingTop: -20, height: 50, backgroundColor: 'white'}}>
+          <Body>
+            <Segment style={{backgroundColor: 'white'}}>
+              <Button first active><Icon name="arrow-back" /></Button>
+              <Button ><Icon name="arrow-back" /></Button>
+              <Button ><Icon name="arrow-back" /></Button>
+              <Button last><Icon name="arrow-forward" /></Button>
+            </Segment>
+          </Body>
+        </Header>
         <Fab
           active={this.state.active}
           direction="up"
